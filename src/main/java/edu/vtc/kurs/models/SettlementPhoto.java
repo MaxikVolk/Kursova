fc5dceea1b43c32d25f9ab9ab9848856859282b2
MaxikVolk <vovchuk.maxim.m@gmail.com>
2022-09-30 10:50:09 +0300

    Photos preload added

---
+++ src/main/java/edu/vtc/kurs/models/SettlementPhoto.java
@@ -0,0 +1,27 @@
+package edu.vtc.kurs.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "settlement_photos")
+public class SettlementPhoto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @ManyToOne
+    @JoinColumn(name = "settlement_id", referencedColumnName = "id")
+    private Settlement settlement;
+    @Column(name = "photo_url")
+    private String photoUrl;
+}
